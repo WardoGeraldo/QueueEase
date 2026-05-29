4d0b9645be591ad59f95c7f0bc802fecb907fa90
WardoGeraldo <egeraldo@student.ciputra.ac.id>
2026-05-29 14:29:39 +0700

    Initial Commit

---
+++ QueueEase/QueueEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueueEaseApp.swift
+//  QueueEase
+//
+//  Created by Edward Geraldo Kristian on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct QueueEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
