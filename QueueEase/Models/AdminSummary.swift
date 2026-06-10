d2bcdce8a40df829f37808a154c9861b98ac2922
WardoGeraldo <egeraldo@student.ciputra.ac.id>
2026-06-10 18:46:33 +0700

    feat: added admin models, and queue models

--- QueueEase/Models/AdminSummary.swift
+++ QueueEase/Models/AdminSummary.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct AdminSummary: Codable, Hashable {
+    let totalTickets: Int
+    let waitingTickets: Int
+    let calledTickets: Int
+    let checkedInTickets: Int
+    let totalCustomers: Int
+    let totalStaff: Int
+    let totalAdmins: Int
+    let activeCategories: Int
+    let activeCounters: Int
+    let todayTickets: Int?
+    let completedTickets: Int?
+}
--- QueueEase/Models/AdminUser.swift
+++ QueueEase/Models/AdminUser.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct AdminUser: Codable, Identifiable, Hashable {
+    let userId: Int
+    let name: String
+    let username: String
+    let email: String?
+    let role: String
+    let isActive: Bool?
+    let createdAt: String?
+    let updatedAt: String?
+
+    var id: Int {
+        userId
+    }
+}
